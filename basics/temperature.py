50241400c73de30b39c09f5eace2f02bcfda069d
Liroy <liroykimhi123@gmail.com>
2024-02-13 15:09:40 +0200

    the temperature practice changed so i updated it

--- basics/temperature.py
+++ basics/temperature.py
@@ -1,3 +1,20 @@
-num = int(input("how many degrees celsius is it: "))
+num = 0
 
-print(num*1.8+32)
+answer = input("what would you like to convert to?")
+
+if answer == "celsius":
+    num = int(input("how many degrees ferenheit is it?"))
+elif answer == "ferenheit":
+    num = int(input("how many degrees celsius is it?"))
+else:
+    print("the answer is not valid")
+
+cel = num*1.8+32
+fer = (num-32)*(5/9)
+
+if answer == "celsius":
+    print(fer)
+elif answer == "ferenheit":
+    print(cel)
+else:
+    print(" ")
